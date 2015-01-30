1d6211342c3eee6b061624f20e2dbf26ecdfb704
cpf5193 <cpf5193@cs.washington.edu>
2015-01-29 17:45:26 -0800

    Successful listener

--- proxy.js
+++ proxy.js
@@ -1,2 +1,12 @@
-var http = require('http');
-var tls = require('tls');
+var net = require('net');
+
+var listener = net.createServer(function(socket) {
+  socket.on('data', function(msg) {
+    console.log('msg: ' + msg);
+  });
+  socket.on('connect', function(msg) {
+    console.log('msg: ' + msg);
+  });
+});
+
+listener.listen(1520);
