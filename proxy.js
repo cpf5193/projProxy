14634e2342e6547446dad91c68b5c66e69d0c478
cpf5193 <cpf5193@cs.washington.edu>
2015-01-30 18:36:29 -0800

    Able to get a response from server

--- proxy.js
+++ proxy.js
@@ -1,10 +1,12 @@
 var net = require('net');
+var dns = require('dns');
+
+// use commandline arguments to set port
 
 var listener = net.createServer(function(socket) {
   socket.on('data', function(msg) {
     var string = msg.toString();
-    // Switch to \r || \r\n
-    var lines = string.split(/[\r\n]/);
+    var lines = string.split(/\r[\n]?/);
     var firstLine = lines[0];
     console.log('<<< ' + firstLine);
     var modifiedString = modifyRequest(msg);
@@ -25,15 +27,17 @@ var listener = net.createServer(function(socket) {
         port = uriPort.match(/:[\d]+/)
         port = port.substring(1, port.length);
     } else {
-      var protocol = firstLine.match(/^http[s]?:/);
-      if (protocol == 'http') {
+      console.log('firstLine: ' + firstLine);
+      var protocol = firstLine.split(' ')[1].match(/^http[s]?:/);
+      console.log('protocol: ' + protocol);
+      if (protocol == 'http:') {
         port = '80';
       } else {
         port = '443';
       }
     }
     console.log('host: ' + hostName + ", port: " + port);
-    // Forward the new message on to the server
+    forwardMessage(hostName, port, modifiedString);
   });
   socket.on('connect', function(msg) {
     console.log('msg: ' + msg);
@@ -47,6 +51,31 @@ function modifyRequest(msg) {
   modString = modString.replace(/ HTTP\/[12].[10]/, ' HTTP/1.0');
   console.log('Modified request: ' + modString);
   return new Buffer(modString);
+}
+
+function forwardMessage(hostName, port, message) {
+  // Create a client connection to send to the server
+  var socket = new net.Socket();
+  var hostIp;
+  dns.resolve4('www.google.com', function(err, addresses) {
+    if (!err && addresses.length > 0) {
+      hostIp = addresses[0];
+      console.log('addresses: ' + JSON.stringify(addresses));
+    } else {
+      console.log('error');
+      exit();
+    }
+    console.log('port: ' + port);
+    console.log('hostIp: ' + hostIp);
+    socket.connect(parseInt(port), hostIp, function() {
+      console.log('writing to socket: ' + message);
+      socket.write(message);
+    });
+    socket.on("data", function(data) {
+      console.log('data from server: ' + data);
+      console.log('Server logic here');
+    });
+  });
 }
 
 listener.listen(1520);
