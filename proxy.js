9193461344fbff1485b339a3b340ff5abb599bc4
cpf5193 <cpf5193@cs.washington.edu>
2015-02-08 15:02:03 -0800

    Edit formatting, remove unused comments, stop writing after client closes

--- proxy.js
+++ proxy.js
@@ -3,14 +3,14 @@ var dns = require('dns');
 var util = require('util');
 
 
-// TODO: find some way to not have to maintain state
 var tunnels = {};
 
 ///////////////////////////////////////////////////
 // Process command line arguments
 ///////////////////////////////////////////////////
 if (process.argv.length != 4) {
-  console.log("Usage: node proxy.js <host> <port>");
+  console.log("Usage: node proxy.js <host> <port> (in run script)\n" +
+              "Usage: ./run <port> (using script)");
   process.exit(1);
 }
 var HOST = process.argv[2];
@@ -24,8 +24,6 @@ var listener = net.createServer({allowHalfOpen: true}, function(socket) {
     handleClientData(socket, msg);
   });
   socket.on('end', function() {
-    util.log('client disconnected');
-    // TODO: send the ending signal to the server
     socket.end();
   });
   socket.on('error', function(err) {
@@ -57,11 +55,9 @@ function modifyRequest(msg) {
 /////////////////////////////////////////////////////////////
 function forwardMessage(hostName, port, message, clientSocket) {
   // Create a client connection to send to the server
-  var socket = new net.Socket();
+  var socket = new net.Socket({allowHalfOpen: true});
   try {
     socket.connect(parseInt(port), hostName, function() {
-      util.log('Forwarding message to ' + hostName + ":" + port + "\n");
-      //util.log('writing to socket: \n' + message + "\n");
       try {
         socket.write(message);
       } catch (ex) {
@@ -70,57 +66,48 @@ function forwardMessage(hostName, port, message, clientSocket) {
     });
   } catch (ex) {
     console.log(ex);
-    // Return 502 error
   }
   
   socket.on("data", function(data) {
-    //util.log('data from server:\n ' + data.toString().substring(0, 1000) + "\n");
     try {
-      clientSocket.write(data);
+      if (clientSocket.writable) {
+        clientSocket.write(data);
+      }
     } catch (ex) {
-      util.log('error writing to server: ' + ex);
+      util.log('error writing back to client: ' + ex);
     }
   });
   socket.on('end', function() {
-    util.log('server ended');
     socket.end();
     clientSocket.end();
-    // TODO: Send end to client
   });
   socket.on('error', function(err) {
     console.log('forward message: ' + err);
   });
   socket.on('close', function() {
-    util.log('server closed');
     socket.end();
     clientSocket.end();
-    // TODO: Send close to client
   });
 }
 
 function createTunnel(hostname, port, msg, clientSocket) {
-  console.log('creating tunnel');
-  var socket = new net.Socket();
+  var socket = new net.Socket({allowHalfOpen: true});
   var clientId = getSocketNameAndPort(clientSocket);
   
   try {
     socket.connect(parseInt(port), hostName, function() {
       // Send success message
       clientSocket.write(new Buffer('HTTP/1.0 200 OK\n\n'));
-      console.log('connected tunnel to ' + hostName + ":" + port);
-      //util.log('writing to client socket: \n' + msg + "\n");
-      //util.log("Creating tunnel for " + getSocketNameAndPort(clientSocket));
       tunnels[clientId] = socket;//new socket for tunnel
     });
   } catch (ex) {
-    console.log(ex);
+    console.log('tunnel connect: ' + ex);
     clientSocket.write(new Buffer('HTTP/1.0 502 Bad Gateway\n\n'));
   }
   socket.on("error", function (err) {
     console.log('create tunnel: ' + err);
   });
   socket.on("data", function(data) {
-    //util.log('data from tunnel:\n ' + data.toString().substring(0, 500) + "\n");
     try {
       clientSocket.write(data);
     } catch (ex) {
@@ -128,25 +115,19 @@ function createTunnel(hostname, port, msg, clientSocket) {
     }
   });
   socket.on('end', function() {
-    util.log('server ended');
     socket.end();
     clientSocket.end();
     delete tunnels[clientId];
-    // TODO: Send end to client
   });
   socket.on('close', function() {
-    util.log('server closed');
     socket.end();
     clientSocket.end();
     delete tunnels[clientId];
-    // TODO: Send close to client
   });
 }
 
 function handleTunnel(clientId, msg) {
   var serverSocket = tunnels[clientId];
-  //console.log("POST-CONNECT " + clientId + " + writing: \n");
-  //console.log(msg.toString());
   serverSocket.write(msg);
 }
 
@@ -158,8 +139,6 @@ function handleTunnel(clientId, msg) {
 /////////////////////////////////////////////////////////////
 function handleClientData(clientSocket, msg) {
   var clientId = getSocketNameAndPort(clientSocket);
-  //util.log("Data from " + clientId);
-  //util.log("Tunnels: " + JSON.stringify(Object.keys(tunnels)));
   var test = tunnels[clientId];
   if(tunnels[clientId] !== undefined) {
     handleTunnel(clientId, msg);
@@ -169,23 +148,17 @@ function handleClientData(clientSocket, msg) {
   var lines = string.split(/\r[\n]?/);
   var firstLine = lines[0];
   var firstLineTokens = firstLine.split(" ");
-  //util.log('>>> ' + firstLineTokens[0] + " " + firstLineTokens[1]);
+  if (lines.length > 1)
+    util.log('>>> ' + firstLineTokens[0] + " " + firstLineTokens[1]);
   var hostAndPort = getHostAndPort(lines);
 
-  // TODO: Need to determine how the proxy knows that it needs to send a message
-  // to a tunnel -- is it all subsequent requests after a connect? In that case
-  // Only one connect is ever honored by the proxy
-  // Is it all subsequent requests to a certain server and port? if so, how do 
-  // we keep track of the tunnels we have open? what is each tunnel socket listening to?
-  
   // If there is no host and port, and there is no registered tunnel,
   // we know that the message is a lagging request to a tunnel that has been
   // shut down, ignore it
-  if (hostAndPort !== undefined) {
+  if (hostAndPort != undefined) {
     if (firstLineTokens[0] == 'CONNECT') {
       createTunnel(hostAndPort['hostName'], hostAndPort['port'], msg, clientSocket);
     } else {
-      console.log('regular message');
       var modifiedBuffer = modifyRequest(msg);
       forwardMessage(hostAndPort['hostName'], hostAndPort['port'], modifiedBuffer, clientSocket);
     }
@@ -229,9 +202,7 @@ function getHostAndPort(lines) {
       port = uriPort.match(/:[\d]+/);
       port = port.substring(1, port.length);
   } else {
-    //util.log('firstLine: ' + firstLine);
     var protocol = firstLineTokens[1].match(/^http[s]?:/);
-    //util.log('protocol: ' + protocol);
     if (protocol == 'http:') {
       port = '80';
     } else {
